24df6fda521222d9a8406b8122a5be8be9049612
Edward Kihiro <kihiroedward@gmail.com>
2024-07-15 08:37:18 +0300

    Minor changes

--- search.py
+++ search.py
@@ -6,9 +6,9 @@ client = Client('en-US')
 
 async def main():
     client.load_cookies('cookies.json')
-       
-    tweets = await client.search_tweet(query='#Tubonge',product='Top',count=10)
-
+    #    RutoMustStay stopSabotagingRuto dealdone
+    tweets = await client.search_tweet(query='RutoMustStay',product='Latest',count=50)
+    print(len(tweets))
     for tweet in tweets:
 
         # Access tweet attributes
@@ -16,7 +16,7 @@ async def main():
             f'id: {tweet.id}',
             f'text {tweet.text}',
             f'favorite count: {tweet.favorite_count}',
-            f'media: {tweet.media}',
+            # f'media: {tweet.media}',
             sep='\n'
         )
     
